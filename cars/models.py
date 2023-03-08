0a8de5c055c0a5b61efb0a2e48ab2fb618cf94ab
ErmanDgrsz <ermandogrusoz@hotmail.com>
2023-03-08 17:15:22 +0300

    cars added to homepage as featured and lates

--- cars/models.py
+++ cars/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-
 from django.db import models
+from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
 
 
 # Create your models here.
@@ -80,11 +81,11 @@ class Car(models.Model):
     )
 
     door_choices = (
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
-        ('6', '6'),
+        (2, 2),
+        (3, 3),
+        (4, 4),
+        (5, 5),
+        (6, 6),
     )
 
     car_title = models.CharField(max_length=255)
@@ -94,12 +95,12 @@ class Car(models.Model):
     year = models.IntegerField('year', choices=year_choice)
     condition = models.CharField(max_length=100)
     price = models.IntegerField()
-    description = models.TextField(max_length=500)
+    description = RichTextField()
     car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    features = models.CharField(choices=features_choices, max_length=100)
+    features = MultiSelectField(choices=features_choices, max_length=500)
     body_style = models.CharField(max_length=100)
     engine = models.CharField(max_length=100)
     transmission = models.CharField(max_length=100)
@@ -109,7 +110,10 @@ class Car(models.Model):
     passengers = models.IntegerField()
     vin_no = models.CharField(max_length=100)
     milage = models.IntegerField()
-    fuel_type = models.CharField(max_length=100)
+    fuel_type = models.CharField(max_length=50)
     no_of_owners = models.CharField(max_length=100)
-    is_featured = models.BooleanField(max_length=100)
+    is_featured = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.car_title
